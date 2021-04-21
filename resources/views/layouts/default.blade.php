f8c94b2946049adff304b7fee6f86f34c3a949ed
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-04-21 11:14:26 -0500

    Update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -572,6 +572,22 @@
 <!-- /jQuery-scripts for Modules (Send Message) -->
 <script async src="//www.instagram.com/embed.js"></script>
 <!-- ...end JS Script -->
-
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '297102598604641');
+    fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=297102598604641&ev=PageView&noscript=1"
+    /></noscript>
+<!-- End Facebook Pixel Code -->
 </body>
 </html>
